6e5e8e80efc83c82a2378ea8dfdeb4b8476844a4
MikhailKrivyakin <mihail_krivyakin@mail.ru>
2021-11-19 11:52:27 +0300

    SVSfixer now works in parallel

--- myscripts/actual/SVSfixer.sh
+++ myscripts/actual/SVSfixer.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
-#delete previos results
-rm result.list
 
+export SHELL=$(type -p bash)
+echo checking...
+function check_svs {
 
-for till in $(cat posclients.list)
-	do
 		#checking every till in list for connection		
-		profuse task run run-command-posclient $till 'Test-NetConnection 10.4.1.251 -Port 10113' > result.tmp
+		profuse task run run-command-posclient $1 'Test-NetConnection 10.4.1.251 -Port 10113' > result_$1.tmp
 		
 		# if output has Fale in it than we try to create new rules and check again		
-		if (( $(cat result.tmp |grep False |wc -l) > 0 )); then
-		    profuse task run run-command-posclient $till 'New-NetFirewallRule -Name "Allow SVS" -Description "Allow SVS" -DisplayName "Allow SVS" -Enabled:True -Profile Public -Direction Outbound -Action Allow -Protocol TCP -RemoteAddress @("10.4.1.251","10.4.2.251") -RemotePort 10113' >> error_svs.log
+		if (( $(cat result_$1.tmp |grep False |wc -l) > 0 )); then
+		    profuse task run run-command-posclient $1 'New-NetFirewallRule -Name "Allow SVS" -Description "Allow SVS" -DisplayName "Allow SVS" -Enabled:True -Profile Public -Direction Outbound -Action Allow -Protocol TCP -RemoteAddress @("10.4.1.251","10.4.2.251") -RemotePort 10113' >> error_svs_$1.log
 			
 			#check again
-			profuse task run run-command-posclient $till 'Test-NetConnection 10.4.1.251 -Port 10113' > result.tmp
+			profuse task run run-command-posclient $1 'Test-NetConnection 10.4.1.251 -Port 10113' > result_$1.tmp
 			#if problem persist then inform user	
 			if (( $(cat result.tmp |grep False |wc -l) > 0 )); then
-					echo "There are some problems with adding firewall rules to the $till . Please chek manualy in the error_svs.log" >> result.list
+					echo "There are some problems with adding firewall rules to the $1 . Please chek manualy in the error_svs_$1.log" 
 			else
 			#if OK - than inform user and delete profuse logs
-					echo "Rules for SVS connection  were added to the $till">>result.list
+					echo "Rules for SVS connection  were added to the $1"
 					rm error_svs.log
 			fi
 		else 
 		#if OK than inform user
-			echo "$till has correct firewall rules for SVS connection." >> result.list
+			echo "$1 has correct firewall rules for SVS connection." 
 		fi
 		#delete this till result
-		rm result.tmp
-	done
-cat result.list
+		rm result_$1.tmp
+	
 
+}
+export -f check_svs
+
+cat posclients.list |parallel --no-notice check_svs {}
 
 #profuse task run run-command-posclient ie0027t008 'Test-NetConnection 10.4.1.251 -Port 10113'
--- myscripts/actual/fjpkg_check.sh
+++ myscripts/actual/fjpkg_check.sh
@@ -5,25 +5,14 @@ export SHELL=$(type -p bash)
 echo -e "\n\n Till:		|		Barclay		|		Globalblue		|		OPOS		|		Lockdown		|"
 echo " -----------------------------------------------------------------------------------------------------------------------------------------------------------------"
 function check_packages {
- 	#lockdown package version
+
 
 		profuse task run run-command-posclient $1 'fjpkg pa' |grep "fujitsu-barclay-smartpay \| fujitsu-opos-drivers\|fujitsu-globalblue-windows\|fujitsu-windows-lockdown"|grep "i |" > tmp.file_$1
-################################################## 
-###################################################			barclay check
 		barclay=$(cat tmp.file_$1 | grep barclay |cut -c 111-125 |sed ' s/ //g')
-################################################## 
-##################################################			Gb check
-		
 		GB=$(cat tmp.file_$1 | grep "fujitsu-globalblue-windows" |cut -c 111-125 |sed ' s/ //g')	
-		
-		
-################################################## 
-##################################################   OPOS check
 		Opos=$(cat tmp.file_$1 | grep "fujitsu-opos-drivers" |cut -c 111-125 |sed ' s/ //g')	
-################################################## 
-################################################## Lockdown check
-		
 		lockdown=$(cat tmp.file_$1 | grep "fujitsu-windows-lockdown-WINDOWS_10_ENTERPRISE_2019_LTSC_1809" |cut -c 111-125 |sed ' s/ //g')
+
 		echo "$1   	|		$barclay	|		$GB		|		$Opos		|		$lockdown			|"
 		
 }
