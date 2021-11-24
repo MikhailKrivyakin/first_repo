ecd17ec22864739ce442328860c3ca8a7dd2af86
MikhailKrivyakin <mihail_krivyakin@mail.ru>
2021-11-24 12:22:16 +0300

    PEDserial script added.

---
+++ myscripts/actual/PEDserial.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export SHELL=$(type -p bash)
+function getserial
+{
+serialnumber=$(profuse task run run-command-posclient $1 '$serial=GCI c:/BarclayCard/SolveConnect/logs/ -name SolveConnectTerminal* |select-object -last 1 |get-content| Select-String -Pattern "Serial number"|select-object -last 1 | out-string; -join ($serial.ToCharArray() |Select-Object -last 30)'|head -n4 |tail -n1)
+
+echo "$1:  $serialnumber"
+
+}
+export -f getserial
+profuse unit show $1| grep Till|cut -c 1-10 | parallel --no-notice getserial {} |sort
